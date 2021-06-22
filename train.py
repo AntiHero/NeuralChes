6cb09b5ea394315b838a62687b737dc376c16084
Aleksandr Schemelev <prophreaker@gmail.com>
2021-06-22 03:10:22 +0300

    add training data + extract moves

--- train.py
+++ train.py
@@ -7,6 +7,12 @@ for file in os.listdir("data"):
 while 1:
     try:
         game = chess.pgn.read_game(pgn)
+        print(game)
     except Exception:
         break
-    print(game)
+
+    board = game.board()
+    for move in game.mainline_moves():
+        board.push(move)
+        print(board)
+    exit(0)
